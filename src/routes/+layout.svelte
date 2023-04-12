4be0271a3342bc222192c15941b31bbe03314ccf
takumiisobe <hi@takumiisobe.com>
2023-04-12 14:13:27 +0900

    0412_1413

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@ onMount(async () => {
 </script>
   
 <svelte:head>
-  <link rel="stylesheet" href="../css/base.css?var=1.18">
+  <link rel="stylesheet" href="../css/base.css?var=1.19">
   <link rel="stylesheet" href="../css/main.css?var=1.17">
   <link rel="stylesheet" href="../css/layout.css?var=1.17">
   <link rel="stylesheet" href="../css/mojikumi.css?var=1.16">
